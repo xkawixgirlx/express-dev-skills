f1a5139c04ab4112bb88218b4bd39aad05714001
xkawixgirlx <jennkosh@gmail.com>
2024-04-09 12:53:25 -0700

    Add Controller Index, connect router to controller

--- routes/skills.js
+++ routes/skills.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const skillsCtrl = require('../controllers/skills');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+/* GET skills listing. */
+router.get('/', skillsCtrl.index);
 
 module.exports = router;
