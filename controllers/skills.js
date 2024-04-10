357f746bbee4d19aa0e2e63a64f5c2392138f0f4
xkawixgirlx <jennkosh@gmail.com>
2024-04-10 13:51:52 -0700

    Add /new route and display form

--- controllers/skills.js
+++ controllers/skills.js
@@ -5,7 +5,12 @@ const Skill = require('../models/skill');
 module.exports = {
     index,
     show,
+    new: newSkill,
 };
+
+function newSkill(req, res) {
+    res.render('skills/new', { title: 'New Skill' });
+}
 
 function show(req, res) {
     res.render('skills/show', {skill: Skill.getOne(req.params.id)});
--- routes/skills.js
+++ routes/skills.js
@@ -5,7 +5,9 @@ const skillsCtrl = require('../controllers/skills');
 
 /* GET skills listing. */
 router.get('/', skillsCtrl.index);
-//GET /skill/:id (show functionality/action => display detail of a single skill)
+// GET /skills/new <-- define BEFORE show Route
+router.get('/new', skillsCtrl.new);
+//GET /skills/:id (show functionality/action => display detail of a single skill)
 router.get('/:id', skillsCtrl.show);
 
 module.exports = router;
